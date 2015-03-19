b74b5e85ecd5cd8c13ece87cb9ed7749528fdbde
joshrounsville <joshuarounsville@gmail.com>
2015-03-18 21:08:41 -0700

    Round 1 dev

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# -buckit-dream
+## Buckit Dream
+
+Setup:
+* install node
+* install gulp
+* cd to root folder
+* run `npm install`
+* to build `gulp`
+* to run dev environment `gulp serve`
---
+++ scripts/plugins.js
@@ -0,0 +1,41 @@
+// Avoid `console` errors in browsers that lack a console.
+(function() {
+    var method;
+    var noop = function () {};
+    var methods = [
+        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+        'timeStamp', 'trace', 'warn'
+    ];
+    var length = methods.length;
+    var console = (window.console = window.console || {});
+
+    while (length--) {
+        method = methods[length];
+
+        // Only stub undefined methods.
+        if (!console[method]) {
+            console[method] = noop;
+        }
+    }
+}());
+
+// Place any jQuery/helper plugins in here.
+
+
+
+//////////////////////////////////////////////// Visible ////////////////////////////////////////////////
+
+$.fn.visible = function(partial) {
+  var $t = $(this),
+      $w = $(window),
+      viewTop = $w.scrollTop(),
+      viewBottom = viewTop + $w.height(),
+      _top = $t.offset().top,
+      _bottom = _top + $t.height(),
+      compareTop = partial === true ? _bottom : _top,
+      compareBottom = partial === true ? _top : _bottom;
+
+  return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+};
